ec4994f88a81bee1bf861714144d1779fdc06a5e
Amirxon <komuljonovamirxon@gmail.com>
2025-12-03 17:48:51 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,25 @@
+# print(chr(128512))
+# print(pow(8,2,9))
+# #
+# harf = 'B'
+# print(ord(harf))  # 66
+# print(chr(ord(harf)))  # B
+#
+# #
+# def celsius_to_fahr(temp):
+#     return 9/5 * temp + 32
+# print(celsius_to_fahr(temp=1))
+#
+# def square(number):
+#     return number * number
+# def cube(number):
+#     return number * square(number)
+# print(square(2))
+# print(cube(3))
+#
+# def salom(ism, fam,ot):
+#     return f"assalomu aleykum {ism} {fam} {ot}, hush kelibsiz"
+# ism = input("ism:")
+# fam = input("fam:")
+# ot = input("ot:")
+# print(salom(ism,fam,ot))
